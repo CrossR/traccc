fadb502f80f0ecd49629bc12e9b59e41145a4d88
CrossR <ryan.cross@warwick.ac.uk>
2025-03-03 12:02:35 +0000

    Start to clean up for PR.

--- device/alpaka/include/traccc/alpaka/utils/get_vecmem_resource.hpp
+++ device/alpaka/include/traccc/alpaka/utils/get_vecmem_resource.hpp
@@ -1,7 +1,7 @@
 /**
  * traccc library, part of the ACTS project (R&D line)
  *
- * (c) 2025 CERN for the benefit of the ACTS project
+ * (c) 2024-2025 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
