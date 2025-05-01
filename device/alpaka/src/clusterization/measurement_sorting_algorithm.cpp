13ab07be122aa83539bb97c8fb11de3dac2bb0b3
CrossR <ryan.cross@warwick.ac.uk>
2025-05-01 15:57:15 +0100

    Start upgrading algorithms to pass around wrapped queue.

--- device/alpaka/src/clusterization/measurement_sorting_algorithm.cpp
+++ device/alpaka/src/clusterization/measurement_sorting_algorithm.cpp
@@ -14,12 +14,17 @@
 namespace traccc::alpaka {
 
 measurement_sorting_algorithm::measurement_sorting_algorithm(
-    vecmem::copy& copy, std::unique_ptr<const Logger> logger)
-    : messaging(std::move(logger)), m_copy{copy} {}
+    const traccc::memory_resource& mr, vecmem::copy& copy, queue& q,
+    std::unique_ptr<const Logger> logger)
+    : messaging(std::move(logger)), m_mr{mr}, m_copy{copy}, m_queue{q} {}
 
 measurement_sorting_algorithm::output_type
 measurement_sorting_algorithm::operator()(
     const measurement_collection_types::view& measurements_view) const {
+
+    // TODO: This will be needed for setting up thrust / oneDPL later.
+    // // Get a convenience variable for the queue that we'll be using.
+    // auto queue = details::get_queue(m_queue);
 
     // Get the number of measurements. This is necessary because the input
     // container may not be fixed sized. And we can't give invalid pointers /
@@ -28,15 +33,25 @@ measurement_sorting_algorithm::operator()(
         m_copy.get().get_size(measurements_view);
 
     // Sort the measurements in place
-#if defined(ALPAKA_ACC_GPU_CUDA_ENABLED) || defined(ALPAKA_ACC_GPU_HIP_ENABLED)
-    auto thrustExecPolicy = thrust::device;
+#if defined(ALPAKA_ACC_GPU_CUDA_ENABLED)
+    auto stream = reinterpret_cast<cudaStream_t>(
+        m_queue.get().deviceNativeQueue());
+    auto execPolicy =
+        thrust::cuda::par_nosync(std::pmr::polymorphic_allocator(&(m_mr.main)))
+            .on(stream);
+#elif defined(ALPAKA_ACC_GPU_HIP_ENABLED)
+    auto stream = reinterpret_cast<hipStream_t>(
+        m_queue.get().deviceNativeQueue());
+    auto execPolicy = thrust::hip_rocprim::par_nosync(
+        std::pmr::polymorphic_allocator(&(m_mr.main)))
+        .on(stream);
 #else
-    auto thrustExecPolicy = thrust::host;
+    auto execPolicy = thrust::host;
 #endif
 
-    thrust::sort(thrustExecPolicy, measurements_view.ptr(),
-                 measurements_view.ptr() + n_measurements,
-                 measurement_sort_comp());
+                thrust::sort(execPolicy, measurements_view.ptr(),
+                             measurements_view.ptr() + n_measurements,
+                             measurement_sort_comp());
 
     // Return the view of the sorted measurements.
     return measurements_view;
