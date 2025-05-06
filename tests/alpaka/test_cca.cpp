bb481d57af67c22bfea41d6b315bc17251cac53d
CrossR <ryan.cross@warwick.ac.uk>
2025-05-06 10:34:12 +0100

    Update tests + examples.

--- tests/alpaka/test_cca.cpp
+++ tests/alpaka/test_cca.cpp
@@ -13,6 +13,7 @@
 #include "tests/cca_test.hpp"
 #include "traccc/alpaka/clusterization/clusterization_algorithm.hpp"
 #include "traccc/alpaka/utils/vecmem_objects.hpp"
+#include "traccc/alpaka/utils/queue.hpp"
 #include "traccc/geometry/silicon_detector_description.hpp"
 
 namespace {
@@ -31,7 +32,7 @@ cca_function_t get_f_with(traccc::clustering_config cfg) {
         vecmem::memory_resource& device_mr = vo.device_mr();
         vecmem::copy copy = vo.copy();
 
-        traccc::alpaka::clusterization_algorithm cc({device_mr}, copy, cfg);
+        traccc::alpaka::clusterization_algorithm cc({device_mr}, copy, queue, cfg);
 
         traccc::silicon_detector_description::buffer dd_buffer{
             static_cast<
